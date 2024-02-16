bcd06844a0b04b302bcd3e625aa3d9649d580bfb
andrew-pineiro <apineiro@towerpower.com>
2024-02-15 23:02:21 -0500

    updates

--- src/main.ps1
+++ src/main.ps1
@@ -9,13 +9,16 @@ $MatchPattern = "^\s*#TODO: (.+)"
 if($Path -eq "." -or $Path -eq "*") {
     $Path = (Get-ChildItem -Recurse $PWD | Where-Object {$_.PSIsContainer -eq $false}).FullName
 }
-
 $Path | ForEach-Object {
-    $Match = (Get-Content $_ | Select-String -Pattern $MatchPattern).Line
-    if($Match) {
+    Get-Content $_ | Select-String -Pattern $MatchPattern | ForEach-Object {
+        $Match = $_.Line
         $IssueTitle = $Match.Substring($Match.IndexOf(':')+2)
-        $IssueTitle
-        gh issue create --title $IssueTitle
+        if(-not((gh issue list) -like "*$IssueTitle*")) {
+            gh issue create --title "[Automated] $IssueTitle" --body "Created On: $(Get-Date)"
+            Write-Host "Issue $IssueTitle Created"
+        } else {
+            #TODO: Implement checking for closed issues
+        }
     }
 
 }
--- test/test_v1.ps1
+++ test/test_v1.ps1
@@ -1,4 +1,6 @@
 #TODO: Test this app
+#TODO: Test it again
+#TODO: And again
 Get-ChildItem $env:ProgramFiles
 
 
