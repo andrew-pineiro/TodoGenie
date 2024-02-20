1c3a49228b26ae3873c581a31d54c192aa29568e
andrew-pineiro <apineiro@towerpower.com>
2024-02-20 08:58:31 -0500

    added feedback to build script

--- build.ps1
+++ build.ps1
@@ -1,22 +1,33 @@
 #Requires -RunAsAdministrator
+$Timer = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+$Timer.Start()
+
 $Separator = [System.IO.Path]::DirectorySeparatorChar
 $ModulePath = $env:ProgramFiles + $Separator + "WindowsPowerShell" + $Separator + "Modules"
 $ModuleName = $PSScriptRoot.Split($Separator)[-1]
 $ModulePathFull = $ModulePath + $Separator + $ModuleName + $Separator
+Write-Debug "Attempting to use $ModulePath as module directory"
 
 if($ModulePath -notin $env:PSModulePath.Split(';')) {
     $ModulePath = $env:PSModulePath[0]
+    Write-Debug "Original ModulePath not found in PSModulePath, reassigning to $ModulePath"
 }
 
 try {
-
     if(Test-Path $ModulePathFull) {
         Remove-Item $ModulePathFull -Recurse -Force -ErrorAction:Stop
+        Write-Debug "Found and removed $ModulePathFull"
     }
     New-Item $ModulePathFull -ItemType Directory | Out-Null
-    Copy-Item .\src\* $ModulePathFull -Recurse -Force -ErrorAction:Stop
+    Write-Debug "Created $ModulePathFull"
 
+    Copy-Item .\src\* $ModulePathFull -Recurse -Force -ErrorAction:Stop
+    Write-Debug "Copying source to $ModulePathFull"
+
+    $Timer.Stop()
+    Write-Host "+ Build completed in $($Timer.Elapsed.TotalSeconds) seconds"
 } catch {
-    Write-Error $_
+    Write-Error "- $_"
+    $Timer.Stop()
     break 1
 }
