219d83dab6d4acfcff34d2e7d962cbbb9432c282
andrew-pineiro <apineiro@towerpower.com>
2024-03-04 22:42:31 -0500

    fixed bug introducted in #110

--- src/Private/Get-AllGitIssues.ps1
+++ src/Private/Get-AllGitIssues.ps1
@@ -5,15 +5,15 @@ function Get-AllGitIssues {
         [ValidateSet('open','closed','all')]
         $State = 'open'
     )
-    $ApiKey = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-content ($secretsPath + $directorySeparator + $secretsFile) | ConvertFrom-Json).GithubApiKey))
-    if(-not($ApiKey) -or $ApiKey -eq "") {
-        Write-Error "invalid apiKey or apiKey not found."
+    $ApiKey = Get-ApiKey
+    $GitData = Get-Content ($RootDirectory + $directorySeparator + ".git" + $directorySeparator + "config") | 
+                    Select-String "url = https://github.com/(.+)/(.+).git"
+
+    if(-not($GitData.Matches)) {
+        Write-Error "unable to gather items from .git config file"
         break 1
     }
-
-    $GitData = (Get-Content ($RootDirectory + $directorySeparator + ".git" + $directorySeparator + "config") | 
-                    Select-String "url = https://github.com/(.+)/(.+).git").Matches
-    
+    $GitData = $GitData.Matches
     $RepoName = $GitData.Groups[2].Value
     $OwnerName = $GitData.Groups[1].Value
     if($RepoName -eq "" -or $null -eq $RepoName -or $OwnerName -eq "" -or $null -eq $OwnerName) {
@@ -28,20 +28,22 @@ function Get-AllGitIssues {
         "X-GitHub-Api-Version" = "2022-11-28"
     }
     try {
-        $Response = Invoke-WebRequest -Uri "$($BaseUri)?state=closed" -Headers $Headers -Method Get
+        $Response = Invoke-WebRequest -Uri "$($BaseUri)?state=closed" -Headers $Headers -Method Get 
         $ResponseHeaders = $Response.Headers
         $ResponseData = $Response.Content | ConvertFrom-Json
-        $PageCount = $ResponseHeaders.Link.Split(',')[1].ToString().Trim() | Select-String -Pattern "^.+page=(\d*).*$"
         $AttemptsLeft = $ResponseHeaders."X-Ratelimit-Remaining"
         Write-Debug "Ratelimit attempts remaining: $AttemptsLeft"
-        if($PageCount) {
-            $PageCount = $PageCount.Matches.Groups[1].Value
-
-            Write-Debug "Page Count: $PageCount"
-            for ($i = 2; $i -le $PageCount; $i++) {
-                Write-Debug "Working on page $i"
-                $Response = Invoke-WebRequest -Uri "$($BaseUri)?state=closed&page=$i" -Headers $Headers -Method Get
-                $ResponseData += $Response.Content | ConvertFrom-Json
+        if($ResponseHeaders.Link) {
+            $PageCount = $ResponseHeaders.Link.Split(',')[1].ToString().Trim() | Select-String -Pattern "^.+page=(\d*).*$"
+            if($PageCount) {
+                $PageCount = $PageCount.Matches.Groups[1].Value
+    
+                Write-Debug "Page Count: $PageCount"
+                for ($i = 2; $i -le $PageCount; $i++) {
+                    Write-Debug "Working on page $i"
+                    $Response = Invoke-WebRequest -Uri "$($BaseUri)?state=closed&page=$i" -Headers $Headers -Method Get
+                    $ResponseData += $Response.Content | ConvertFrom-Json
+                }
             }
         }
     } catch {
