e06b8b38ab13aaf192cac4701fa2e343beb9de32
andrew-pineiro <apineiro@towerpower.com>
2024-03-07 21:26:32 -0500

    2

--- src/Private/Get-ApiKey.ps1
+++ src/Private/Get-ApiKey.ps1
@@ -1,8 +1,9 @@
 function Get-ApiKey() {
+    Write-Debug "Attempting to get key from $($secretsPath + $directorySeparator + $secretsFile)"
     $jsonData = Get-content ($secretsPath + $directorySeparator + $secretsFile) | ConvertFrom-Json
     if(($jsonData).GithubApiKey) {
         return [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(($jsonData).GithubApiKey))
     }
-    Write-Error "invalid apiKey or apiKey not found. $jsonData"
+    Write-Error "invalid apiKey or apiKey not found."
     break 1
 }
