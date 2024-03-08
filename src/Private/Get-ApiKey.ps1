8891e12adb0b6f2d9cc5bcc8de61353376cbf82b
andrew-pineiro <apineiro@towerpower.com>
2024-03-07 21:28:24 -0500

    3

--- src/Private/Get-ApiKey.ps1
+++ src/Private/Get-ApiKey.ps1
@@ -1,5 +1,5 @@
 function Get-ApiKey() {
-    Write-Debug "Attempting to get key from $($secretsPath + $directorySeparator + $secretsFile)"
+    Write-Host "Attempting to get key from $($secretsPath + $directorySeparator + $secretsFile)"
     $jsonData = Get-content ($secretsPath + $directorySeparator + $secretsFile) | ConvertFrom-Json
     if(($jsonData).GithubApiKey) {
         return [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(($jsonData).GithubApiKey))
