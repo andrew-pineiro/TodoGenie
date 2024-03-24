a782a32177f05c649ab765be948ffdca0415aad4
andrew-pineiro <apineiro@towerpower.com>
2024-03-23 20:31:14 -0400

    trying to get other subcommands working #3

--- src/Private/Get-ApiKey.ps1
+++ src/Private/Get-ApiKey.ps1
@@ -1,6 +1,7 @@
 function Get-ApiKey() {
     Write-Debug "Secret file $secretsFile from path: $secretsPath"
     $jsonData = Get-content ($secretsPath + $directorySeparator + $secretsFile) | ConvertFrom-Json
+    Write-Debug "File Data: $jsonData"
     if(($jsonData).GithubApiKey) {
         return [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(($jsonData).GithubApiKey))
     } else {
