4198aa9831da5086f844c68975e6dd354d0db972
andrew-pineiro <chillsav12345@gmail.com>
2025-02-23 23:27:54 -0500

    initial c# setup

---
+++ src/console/Program.cs
@@ -0,0 +1,14 @@
+﻿FileHandler handler = new();
+Error.LogDirectory = "C:\\Users\\Chill\\Repositories\\TodoGenie\\.logs";
+if(args.Length < 1) {
+    Console.WriteLine("Usage: .\\TodoGenie.exe [git directory]");
+    Error.Write("insufficent arguments supplied");
+}
+var dir = args[0];
+
+var files = handler.GetAllValidFiles(dir);
+foreach(var file in files) {
+     Console.WriteLine("---------------------");
+     Console.WriteLine(file);
+     Console.WriteLine("---------------------");
+}
